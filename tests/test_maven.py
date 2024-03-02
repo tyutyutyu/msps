0e6d095239f08586c3a7d18916ffe29e9075db62
István Földházi <istvan.foldhazi@gmail.com>
2024-03-02 21:21:30 +0100

    Update test script and add coverage configuration

--- tests/test_maven.py
+++ tests/test_maven.py
@@ -5,10 +5,13 @@ from unittest import mock
 from msps.maven import Maven
 
 
+HOME = os.environ["HOME"]
+
+
 class TestMaven(unittest.TestCase):
     @mock.patch.dict(os.environ, {"M2_HOME": "/custom/path"})
     def test_M2_HOME1(self) -> None:
         self.assertEqual(Maven().m2_home, "/custom/path")
 
     def test_M2_HOME2(self) -> None:
-        self.assertEqual(Maven().m2_home, "/home/app/.m2")
+        self.assertEqual(Maven().m2_home, f"{HOME}/.m2")
